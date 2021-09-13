afad419b65605ed1529fa09c869d8c2c1ea6034f
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-09-14 03:09:30 +0900

    feat: show ui

--- src/run.py
+++ src/run.py
@@ -1,3 +1,15 @@
+from gui import ui_support
+
+
+class Main:
+    def __init__(self):
+        pass
+
+    def main(self):
+        ui_support.setup_ui()
+        ui_support.show_ui()
+
+
 if __name__ == '__main__':
-    from gui import ui_support
-    ui_support.show_ui()
+    main = Main()
+    main.main()
